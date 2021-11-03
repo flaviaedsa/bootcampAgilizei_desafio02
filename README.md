467b07a5d582082d01481b04a66f66fb612413a8
flaviaedsa <flaviaedsa@hotmail.com>
2021-11-02 23:12:23 -0300

    refatoracoes de melhoria

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
-![Cypress](https://pasteboard.co/DqmWlXbKt3ig.png)
 
 ## BootcampAgilizei_Desafio02_Cadastro_no_site_de_compras "automationpractice.com"
 
 ## Tecnologia
 
-* Node versão
-* Cypress versão
+* Node versão v14.16.1
+* Cypress
+* Chance (Geração de dados aleatórios)
+* GH Actions (CI)
+* Mochawesone (Relatório)
 
 ## Instalação
 
-* To install gems:
->    $ npm install
+>   $ Faça o clone do projeto e dentro da pasta siga o comando abaixo
+>   $ npm install
 
 ## Execução
 
->    $ npx cypress open
+>   $ npx cypress open (abrindo o browser)
+>   $ npx cypress run (modo headless)
 
 ## Autora
  
--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -10,18 +10,17 @@ describe('Cadastro', () => {
         cy.get('.login').click()
 
         cy.get('input#email_create').type(chance.email())
-
         cy.get('button#SubmitCreate').click()
 
         // PERSONAL INFORMATION
      
-        cy.get('input#id_gender2').click()
+        cy.get('input#id_gender2').check()
         cy.get('input#customer_firstname').type(chance.first())
         cy.get('input#customer_lastname').type(chance.last())
         cy.get('input#passwd').type(chance.string({ length: 5 }))
-        cy.get('select#days').select('10', {force: true})
-        cy.get('select#months').select('May', {force: true})
-        cy.get('select#years').select('2000', {force: true})
+        cy.get('select#days').select(chance.natural({min: 1, max: 28}))
+        cy.get('select#months').select(chance.month())
+        cy.get('select#years').select(chance.year({ min: 1950, max: 2000}))
 
         // YOUR ADDRESS
    
@@ -29,7 +28,7 @@ describe('Cadastro', () => {
         cy.get('input#address1').type(chance.address())
         cy.get('input#address2').type(chance.address())
         cy.get('input#city').type(chance.city())
-        cy.get('select#id_state').select('Florida', {force: true})
+        cy.get('select#id_state').select(chance.natural({ min: 1, max: 53}))
         cy.get('input#postcode').type(chance.zip())
         cy.get('textarea[id=other]').type('Test')
         cy.get('input#phone').type(chance.phone())
@@ -39,5 +38,6 @@ describe('Cadastro', () => {
         cy.get('button#submitAccount').click()
 
         cy.contains('Welcome to your account.')  
+
     });
 });
